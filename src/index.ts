171056452348c4661cc12dca71aff908ad42d7b1
leeoo8 <leeoo888@outlook.com>
2024-11-22 11:47:54 +0800

    Update index.ts

--- src/index.ts
+++ src/index.ts
@@ -9,17 +9,24 @@ const serveHotApi: (port?: number) => void = (port: number = config.PORT) => {
     const apiServer = serve({
       fetch: app.fetch,
       port,
+      hostname: "0.0.0.0", // 确保监听所有主机地址
     });
     logger.info(`🔥 DailyHot API 成功在端口 ${port} 上运行`);
-    logger.info(`🔗 Local: 👉 http://localhost:${port}`);
+    logger.info(`🔗 可访问地址: http://0.0.0.0:${port}`);
     return apiServer;
   } catch (error) {
-    logger.error(error);
+    logger.error(`❌ 服务启动失败: ${error}`);
   }
 };
 
-if (process.env.NODE_ENV === "development" || process.env.NODE_ENV === "docker") {
-  serveHotApi(config.PORT);
+// 检查环境并启动服务器
+if (
+  process.env.NODE_ENV === "development" ||
+  process.env.NODE_ENV === "docker" ||
+  process.env.NODE_ENV === "production"
+) {
+  const port = parseInt(process.env.PORT || `${config.PORT}`, 10); // 优先使用环境变量 PORT
+  serveHotApi(port);
 }
 
 export default serveHotApi;
